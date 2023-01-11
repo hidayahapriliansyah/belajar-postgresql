f164bf8c8e6ef190bf20b3aac2e360f8004e63d9
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-11 20:17:18 +0700

    nambahin latihan coalescing js

---
+++ latihan-coalescing.js
@@ -0,0 +1,11 @@
+// const name = 'Adi';
+const name = undefined;
+
+const call = NaN || name;
+// const call = name || 'Hidayah';
+
+// const call = NaN && name;
+// const call = name && 'Hidayah';
+
+console.log(call);
+
